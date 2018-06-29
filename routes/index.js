cd8d2082bb0f5e56cb73f10cd926bb85f5a4d1d6
jenny274 <jennifer.ramananarivo@gmail.com>
2018-06-29 11:33:20 +0400

    ajout fonction ajouter un utilisateur

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+var personne = require("../controllers/usersController");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+//cree une personne
+router.get("/creer", personne.create);
+
+//sauvegarder une personne
+router.post("/save", personne.save);
+
+
 
 module.exports = router;
