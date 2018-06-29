dcfcd341ee4abed971c4a4d32b0cb870cf5409c6
alexreu <alexxxx.974@gmail.com>
2018-06-29 10:04:53 +0400

    ajout du controller listing tache

--- controllers/tachesControllers.js
+++ controllers/tachesControllers.js
@@ -1,1 +1,18 @@
 /*controller pour les taches */
+var mongoose = require('mongoose');
+var taches = require('../models/models');
+
+var tachesController = {};
+
+tachesController.list = function(req,res){
+    taches.find({}).exec(function (err, taches) {
+        if (err){
+            console.log('Error : ', err);
+        }else{
+            res.render("../views/planning/taches", {taches:taches} );
+        }
+    });
+};
+
+
+module.exports = tachesController;
